9227ae5f7ac03abc199b9ffdaab31f6ddb3ebdb5
SunMingchun <871069690@qq.com>
2018-10-17 19:21:53 +0800

    提交一个py程序海龟画画

---
+++ draw.py
@@ -0,0 +1,25 @@
+import turtle
+def main():
+    t=turtle.Turtle()
+    t.hideturtle()
+    drawFilledRectangle(t,0,0,150,25,"light blue","light blue")
+    drawFilledRectangle(t,0,25,150,50,"blue","blue")
+    drawFilledRectangle(t,0,75,150,25,"light blue","light blue")
+    drawDot(t,75,50,40,"red")
+def drawFilledRectangle(t,x,y,w,h,colorP="black",colorF="white"):
+    t.pencolor(colorP)
+    t.fillcolor(colorF)
+    t.up()
+    t.goto(x,y)
+    t.down()
+    t.begin_fill()
+    t.goto(x+w,y)
+    t.goto(x+w,y+h)
+    t.goto(x,y+h)
+    t.goto(x,y)
+    t.end_fill()
+def drawDot(t,x,y,diameter,colorP):
+    t.up()
+    t.goto(x,y)
+    t.dot(diameter,colorP)
+main()
